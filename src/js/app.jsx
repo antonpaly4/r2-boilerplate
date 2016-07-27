688120198a6f30bedffc6cc465728eb4e848a065
antonpaly4 <anton.korchagin@takestep.ru>
2016-07-27 17:52:47 +0500

    remove old code

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -5,16 +5,6 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 class App extends React.Component {
 
-  componentWillMount() {
-    if(!getCookie('CSRFToken')){
-      Server.get('api/version', (err,res) => {
-        if(res.status === 200){
-          setCookie('CSRFToken', res.response);
-        }
-      });
-    }
-  }
-
   render() {
     return (
       <div>{this.props.children}</div>
